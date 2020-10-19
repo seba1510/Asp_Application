889fc19d6a0cdca8a6479327742792b8364d26f3
seba1510 <48321015+seba1510@users.noreply.github.com>
2020-10-19 19:04:08 +0200

    Lab_1

---
+++ Asp_Project/Controllers/CompanyController.cs
@@ -0,0 +1,30 @@
+﻿using Asp_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp_Project.Controllers
+{
+	public class CompanyController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(CompanyModel company)
+		{
+			var vievModel = new CompanyAddedVievModel
+			{
+				NumberOfCharsInName = company.Name.Length,
+				NumberOfCharsInDescription = company.Description.Length,
+				IsHidden = !company.IsVisible
+			};
+
+			return View("CompanyAdded", vievModel);
+		}
+	}
+}
---
+++ Asp_Project/Models/CompanyAddedVievModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp_Project.Models
+{
+	public class CompanyAddedVievModel
+	{
+		public int NumberOfCharsInName { get; set; }
+
+		public int NumberOfCharsInDescription { get; set; }
+		public bool	IsHidden { get; set; }
+	}
+}
