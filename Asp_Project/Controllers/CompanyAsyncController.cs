d4a1890ab8e8200955b00eb642a259c6b82b96a5
seba1510 <48321015+seba1510@users.noreply.github.com>
2020-11-23 12:38:58 +0100

    lab2

---
+++ Asp_Project/Controllers/CompanyAsyncController.cs
@@ -0,0 +1,28 @@
+﻿using Asp_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp_Project.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CompanyAsyncController : ControllerBase
+	{
+        [HttpPost]
+        [Route("AddNewItem")]
+        public async Task<IActionResult> AddNewItem(CompanyModel companyModel)
+        {
+            var companyAddedModel = new CompanyAddedVievModel
+            {
+                NumberOfCharsInName = companyModel.Name.Length,
+                NumberOfCharsInDescription = companyModel.Description.Length,
+                IsHidden = !companyModel.IsVisible
+            };
+
+            return Ok(companyAddedModel);
+        }
+    }
+}
--- Asp_Project/Controllers/CompanyController.cs
+++ Asp_Project/Controllers/CompanyController.cs
@@ -11,7 +11,7 @@ namespace Asp_Project.Controllers
 	{
 		public IActionResult Index()
 		{
-			return View();
+			return View("IndexAjax");
 		}
 
 		[HttpPost]
